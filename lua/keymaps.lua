9bb2d2c1cea327794e83f826d85354092c3f84db
Jumas <kbbyfl91@gmail.com>
2024-02-08 12:49:58 +0300

    Add keymap for duplicate tab

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,24 +5,31 @@ map("n", "<C-N>", ":bnext<CR>", opts)
 map("n", "<C-P>", ":bprev<CR>", opts)
 
 -- Neotree
-map("n", "<Leader>t", ":Neotree toggle<CR>", opts)
+map("n", "<Leader>t", ":Neotree toggle<CR>",
+    {table.unpack(opts), desc = "Toggle Neotree"})
 
 -- Neoformat
-map("n", "<Leader>p", ":Neoformat<CR>", opts)
+map("n", "<Leader>p", ":Neoformat<CR>",
+    {table.unpack(opts), desc = "Prettify buffer"})
 
 -- Быстрое сохранение сессии
-map("n", "<C-s>", ":mksession!<CR>", opts)
+map("n", "<C-s>", ":mksession!<CR>",
+    {table.unpack(opts), desc = "Save vim session"})
 
 -- Move to previous/next
-map("n", "<A-,>", ":tabprev<CR>", opts)
-map("n", "<A-.>", ":tabnext<CR>", opts)
+map("n", "<A-,>", ":tabprev<CR>", {table.unpack(opts), desc = "Previous tab"})
+map("n", "<A-.>", ":tabnext<CR>", {table.unpack(opts), desc = "Next tab"})
 
 -- Close buffer
-map("n", "<A-c>", ":tabc<CR>", opts)
+map("n", "<A-c>", ":tabc<CR>", {table.unpack(opts), desc = "Close buffer"})
 
 -- Markdown preview
--- map("n", "<C-p>", ":MarkdownPreviewToggle<CR>", opts)
+-- map("n", "<C-p>", ":MarkdownPreviewToggle<CR>", {table.unpack(opts), desc = "Markdown preview"})
 
 -- Изменение размера vertical split
-map("n", "<A-[>", ":vertical resize +1<CR>", opts)
-map("n", "<A-]>", ":vertical resize -1<CR>", opts)
+map("n", "<A-[>", ":vertical resize +1<CR>",
+    {table.unpack(opts), desc = "Move splitline +1"})
+map("n", "<A-]>", ":vertical resize -1<CR>",
+    {table.unpack(opts), desc = "Move splitline -1"})
+
+map("n", "<A-d>", ":tab split<CR>", {table.unpack(opts), desc = "Duplicate tab"})
