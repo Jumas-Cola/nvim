06712cc654fd1be6b708ce1bf74c3d2ea4cd191a
Jumas <kbbyfl91@gmail.com>
2024-02-08 12:57:29 +0300

    Add keymap for Telescope search in keymaps

--- lua/plugins/config/telescope.lua
+++ lua/plugins/config/telescope.lua
@@ -7,6 +7,7 @@ keymap.set("n", "<leader>ff", builtin.find_files, {desc = "Find files"})
 keymap.set("n", "<leader>fg", builtin.live_grep, {desc = "Live grep"})
 keymap.set("n", "<leader>fb", builtin.buffers, {desc = "Find in buffers"})
 keymap.set("n", "<leader>fh", builtin.help_tags, {desc = "Find in help"})
+keymap.set("n", "<leader>fk", builtin.keymaps, {desc = "Find in keymaps"})
 map("n", "<leader>ft", ":TodoTelescope keywords=TODO,FIX<CR>", {})
 
 telescope.setup {pickers = {find_files = {hidden = true}}}
